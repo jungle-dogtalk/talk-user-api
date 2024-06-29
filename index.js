3e39b2f80a7bc3d6eb89c3d6a3919b08cfa89d4d
PeterChoi <soww12345@gmail.com>
2024-06-29 20:02:07 +0900

    Feat: 테스트용 API 구현

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+const Response = require('./src/dto/response');
+
+// CORS 미들웨어 사용
+app.use(cors());
+app.use(express.json());
+
+// TODO: 테스트용 코드 추후 삭제 요망
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello 2팀 멍톡 나만무!!!');
+  res.send('2팀 나만무 멍톡 백엔드 API 서버');
+});
+
+// data가 1개일 경우 객체 형태로 응답하기
+app.get('/test/single-data', async (req, res) => {
+    
+  await delay(2000);  // 2초 후 응답을 보냅니다
+
+  const data = {'id': 1, 'name': '김철수'};
+  res.json(Response.success(data, "테스트 성공!"));
+});
+
+// data가 여러개일 경우 객체의 배열 형태로 응답하기
+app.get('/test/multiple-data', async (req, res) => {
+
+  await delay(2000);  // 2초 후 응답을 보냅니다
+
+  const data = [
+    {'id': 1, 'name': '김철수'},
+    {'id': 2, 'name': '김영희'},
+  ];
+
+  res.json(Response.success(data, "테스트 성공!"));
+});
+
+app.get('/test/greeting', async (req, res) => {  
+  const data = { 'id': 1, 'name': req.query.name };
+
+  res.json(Response.success(data, "테스트 성공!"));
 });
 
 app.listen(port, () => {

454a7ed455b50b51c1b52404c2b4d8318a295105
PeterChoi <soww12345@gmail.com>
2024-07-01 00:02:57 +0900

    Comment: 공통 응답 파일에 주석 코드 추가

--- src/dto/response.js
+++ src/dto/response.js
@@ -1,4 +1,10 @@
 class ApiResponse {
+  /**
+   * ApiResponse 인스턴스를 생성합니다.
+   * @param {*} data - 응답 데이터
+   * @param {string} [message="성공"] - 응답 메시지
+   * @param {number} [code=200] - HTTP 상태 코드
+   */
     constructor(data, message = "성공", code = 200) {
       this.code = code;
       this.status = code === 200;
@@ -6,13 +12,25 @@ class ApiResponse {
       this.data = data;
     }
   
+  /**
+   * 성공 응답을 생성합니다.
+   * @param {*} data - 응답 데이터
+   * @param {string} [message="성공"] - 성공 메시지
+   * @returns {ApiResponse} 성공 응답 객체
+   */
     static success(data, message = "성공") {
       return new ApiResponse(data, message);
     }
   
+    /**
+     * 에러 응답을 생성합니다.
+     * @param {string} [message="실패"] - 에러 메시지
+     * @param {number} [code=400] - HTTP 에러 코드
+     * @returns {ApiResponse} 에러 응답 객체
+     */
     static error(message = "실패", code = 400) {
       return new ApiResponse(null, message, code);
     }
   }
   
-  module.exports = ApiResponse;
+module.exports = ApiResponse;

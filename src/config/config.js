e3eb76f636c6b430f45688b64b786bd351b776e8
doyeon kim <kim2004on@gmail.com>
2024-07-01 23:48:30 +0900

    Feat: 포트 정의

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URI: process.env.MONGODB_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  OPENVIDU_URL: process.env.OPENVIDU_URL,
+  OPENVIDU_SECRET: process.env.OPENVIDU_SECRET,
+};
+
+export default config;

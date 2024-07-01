00616779eedc857876f606a4d1245f83a595bebe
doyeon kim <kim2004on@gmail.com>
2024-07-02 00:21:28 +0900

    Fix: app.js 수정

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import chatRoutes from './routes/chatRoutes.js';
 import openviduRoutes from './routes/openviduRoutes.js';
 import errorHandler from './middlewares/errorMiddleware.js';
 
@@ -14,8 +12,6 @@ app.use(cors());
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/chats', chatRoutes);
 app.use('/api/openvidu', openviduRoutes);
 
 // Error handling middleware

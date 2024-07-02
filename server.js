098f8f6d22cf09d13cbbd4a83e84a17bc7c92d43
심우정 <dnwjd040635@gmail.com>
2024-07-02 21:44:21 +0900

    Feat: 개인 스크립트와 공통 스크립트 분리

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const errorHandler = require('./src/middlewares/errorMiddleware');
 const BadRequestError = require('./src/errors/BadRequestError');
 const OpenAI = require('openai');
 
+let conversation = []; // 대화 누적할 공통 스크립트
+
 // OpenAI 클라이언트 초기화
 const openai = new OpenAI({
-    apiKey: 'sk-proj-39bA7taZJJXdrGgnVYwZT3BlbkFJ1VyynGCO4t0XeJLDC14S',
+    apiKey: 'sk-proj-IsiexBbYA3UdH6Iddno1T3BlbkFJ4GRSBYmIfWczJrsdr6qf',
 });
 
 app.use(cors()); // CORS 미들웨어 사용 (TODO: CORS 정책 변경 요망 / 현재는 모든 ip에 대해 열려있음)
@@ -84,12 +86,23 @@ app.get(
         res.json(Response.success(null, '테스트 성공'));
     })
 );
+/* ----------------------음성인식 및 주제 특정 관련--------------------- */
+app.post(
+    '/send-text',
+    tryCatch((req, res) => {
+        const { text } = req.body;
+        conversation.push(text);
+        console.log('누적할 텍스트: ', text);
+        res.json(Response.success(null, '텍스트 누적 성공'));
+    })
+);
 
-// 음성인식된 텍스트 기반 답변 생성 API 엔드포인트
-app.post('/ask-ai', async (req, res) => {
-    const userQuery = req.body.query; // 요청 본문에서 음성인식된 텍스트 가져오기
-    console.log(userQuery);
-    try {
+// 음성인식된 텍스트 기반 주제 추천 생성 API 엔드포인트 (음성인식 종료 버튼 누를 시)
+app.post(
+    '/ask-ai/title',
+    tryCatch(async (req, res) => {
+        const userQuery = conversation.join('. '); // 누적된 대화 스크립트 하나의 문장으로 합침
+        console.log('누적된 대화: ', userQuery);
         const startTime = Date.now(); // ai 요청 시작 시간 기록
 
         const completion = await openai.chat.completions.create({
@@ -100,7 +113,7 @@ app.post('/ask-ai', async (req, res) => {
                     role: 'user',
                     content:
                         userQuery +
-                        '\n 해당 대화 흐름에 따른 자세한 주제를 추천해줘. 다른 말 없이 정말 딱 주제만 5가지 정도.',
+                        '\n 해당 대화 맥락에서 이 다음 주제로 대화하기 좋은 주제를 추천해줄 수 있니? 다른 말 없이 5가지 정도의 주제를 선정해줘.',
                 },
             ],
             max_tokens: 300, // 응답 시간 최소화를 위해 조정 가능
@@ -109,12 +122,32 @@ app.post('/ask-ai', async (req, res) => {
         const answer = completion.choices[0].message.content; // AI 응답 가져오기
         const endTime = Date.now(); // ai 요청 종료 시간 기록
         console.log(`AI 응답 시간: ${endTime - startTime}초`); // 응답 시간 출력
+        conversation = []; // 누적 대화 스크립트 초기화
         res.json(Response.success(answer, 'AI 응답 성공')); // 커스텀 응답 객체 사용해 응답 반환
-    } catch (error) {
-        console.error(error);
-        res.json(Response.error('AI 응답 실패'));
-    }
-});
+    })
+);
+
+// 음성인식된 텍스트 기반 관심사 추천 생성 API 엔드포인트 (음성인식 종료 버튼 누를 시)
+app.post(
+    '/ask-ai/interest',
+    tryCatch(async (req, res) => {
+        const { query } = req.body; // 클라이언트
+        const completion = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'user',
+                    content:
+                        query +
+                        '\n 해당 대화의 맥락을 기준으로 화자가 가지고 있는 관심사를 명사로 특정해서 5개만 말해줘. 영어로 말해주었으면 해.',
+                },
+            ],
+            max_tokens: 300,
+        });
+        const answer = completion.choices[0].message.content;
+        res.json(Response.success(answer, 'AI 응답 성공'));
+    })
+);
 
 app.use(errorHandler);
 

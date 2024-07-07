1990f68d204a4b82d472297039d6f0f29094968f
PeterChoi <soww12345@gmail.com>
2024-07-07 13:46:08 +0900

    Feat: CORS 허용 URL 등록 및 UUID 의존성 추가

--- src/app.js
+++ src/app.js
@@ -12,8 +12,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
-        // origin: "https://www.barking-talk.org",
+        origin: ["http://localhost:5173", "https://www.barking-talk.org"],
         mathods: ["GET", "POST"],
     },
 });

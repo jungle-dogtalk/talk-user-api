707c4e521fe07d30d88d8463314bc61cbf246a55
doyeon kim <kim2004on@gmail.com>
2024-07-08 00:54:19 +0900

    Feat: 회원가입 S3로 이미지 업로드 및 DB URL 저장 구현

--- src/models/User.js
+++ src/models/User.js
@@ -8,6 +8,8 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true }, // 이름 필드
     email: { type: String, required: true, unique: true }, // 이메일 필드
     interests: [String], // 관심사 필드 (배열)
+    profileImage: { type: String, required: false }, // 프로필 이미지 URL 필드 (선택)
+    profileImageId: { type: String, required: false }, // 이미지의 UUID를 저장하는 필드 (선택)
 });
 
 // 사용자 저장 전에 비밀번호 해시화
@@ -16,9 +18,9 @@ userSchema.pre('save', async function (next) {
         // 비밀번호가 수정되지 않았다면 다음 미들웨어로 이동
         return next();
     }
+
     const salt = await bcrypt.genSalt(10); // 솔트 생성
     this.password = await bcrypt.hash(this.password, salt); // 비밀번호 해시화
-    console.log('Hashed password:', this.password); // 로그 추가
     next(); // 다음 미들웨어로 이동
 });
 

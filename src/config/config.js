8f1d096f88512986a7515b201fd0cbd602a07770
doyeon kim <kim2004on@gmail.com>
2024-07-08 00:53:28 +0900

    Chore: 환경 변수 S3 추가

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,10 @@ const config = {
     OPENVIDU_URL: process.env.OPENVIDU_URL,
     OPENVIDU_SECRET: process.env.OPENVIDU_SECRET,
     PYTHON_SERVER_URL: process.env.PYTHON_SERVER_URL,
+    AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+    AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
 };
 
 export default config;

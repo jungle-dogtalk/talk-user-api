7aec380d610a72d7db04770f8a526951f2294e8b
joosomi <wnthal1211@gmail.com>
2024-07-09 05:14:57 +0900

    chores: open ai 모듈 설치, fix conflicts

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -52,26 +52,8 @@ export const checkUsername = async (req, res) => {
     }
 };
 
-
-// 아이디 중복 검사 엔드포인트 핸들러
-export const checkUsername = async (req, res) => {
-  const { username } = req.body;
-  try {
-      const exists = await authService.checkUsername(username);
-      res.status(200).json({ 
-        code: 200,
-        status: true,
-        data: exists,
-        message: exists? "중복되는 ID가 있습니다.":"사용가능한 ID입니다."
-       });
-  } catch (error) {
-      console.error('Error during username check:', error);
-      res.status(500).json({ error: error.message });
-  }
-};
-
-  // 로그인 엔드포인트 핸들러
-  export const login = async (req, res, next) => {
+// 로그인 엔드포인트 핸들러
+export const login = async (req, res, next) => {
     try {
         const { username, password } = req.body; // 요청 본문에서 사용자 이름과 비밀번호 추출
 
@@ -96,15 +78,14 @@ export const checkUsername = async (req, res) => {
 
 // 계정 삭제 엔드포인트 핸들러
 export const deleteAccount = async (req, res) => {
-  try {
+    try {
+        const userId = req.user.id; // JWT 토큰에서 사용자 ID 추출
+        await authService.deleteUserById(userId); // 서비스에서 사용자 삭제 로직 호출
 
-      const userId = req.user.id; // JWT 토큰에서 사용자 ID 추출
-      await authService.deleteUserById(userId); // 서비스에서 사용자 삭제 로직 호출
-
-      // 성공 응답 반환
-      res.status(200).json({ message: 'Account deleted successfully' });
-  } catch (error) {
-      console.error('Error deleting account:', error);
-      res.status(500).json({ error: error.message });
-  }
+        // 성공 응답 반환
+        res.status(200).json({ message: 'Account deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting account:', error);
+        res.status(500).json({ error: error.message });
+    }
 };
